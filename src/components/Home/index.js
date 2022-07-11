a5038dae541d7328853a77d9be581d8ddcd57371
Smit Patel <smitp520.sp@gmail.com>
2022-07-10 21:45:37 -0500

    changes made

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -9,28 +9,28 @@ import './index.scss'
 const Home = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
 
-  const nameArray = ['m', 'i', 't']
-  // const jobArray = [
-  //   's',
-  //   'o',
-  //   'f',
-  //   't',
-  //   'w',
-  //   'a',
-  //   'r',
-  //   'e',
-  //   ' ',
-  //   'd',
-  //   'e',
-  //   'v',
-  //   'e',
-  //   'l',
-  //   'o',
-  //   'p',
-  //   'e',
-  //   'r',
-  //   '.',
-  // ]
+  const nameArray = ['m', 'i', 't', ',']
+  const jobArray = [
+    's',
+    'o',
+    'f',
+    't',
+    'w',
+    'a',
+    'r',
+    'e',
+    ' ',
+    'd',
+    'e',
+    'v',
+    'e',
+    'l',
+    'o',
+    'p',
+    'e',
+    'r',
+    '.',
+  ]
 
   useEffect(() => {
     return setTimeout(() => {
@@ -58,11 +58,11 @@ const Home = () => {
               idx={15}
             />
             <br />
-            {/* <AnimatedLetters
+            <AnimatedLetters
               letterClass={letterClass}
               strArray={jobArray}
               idx={22}
-            /> */}
+            />
           </h1>
           <h2>Software Developer / Fullstack Developer</h2>
           <Link to="/contact" className="flat-button">
